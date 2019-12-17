47eba5cced378aa469de7bfa2574d14e350a5ce6
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2019-12-17 10:44:29 +0100

    add which-key

--- emacs-init.el
+++ emacs-init.el
@@ -845,6 +845,20 @@ sufficiently large."
   :ensure t
   :defer t)
 
+;; which-key is a minor mode for Emacs that displays the key bindings following
+;; your currently entered incomplete command (a prefix) in a popup.
+(use-package which-key
+  :ensure t
+  :pin melpa-stable
+  :defer 5 ;; load after 5s
+  :diminish
+  :config
+  ;; show popup at the bottom of the window
+  (which-key-setup-side-window-bottom)
+  ;; delay after which which-key popup appears after starting to type a command
+  (setq which-key-idle-delay 1.0)
+  (which-key-mode))
+
 
 ;;; Finalization
 
