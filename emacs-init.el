172d59d43e05912147c5983cba9275d482887567
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-01-13 08:28:42 +0100

    switch back to wrap long lines

--- emacs-init.el
+++ emacs-init.el
@@ -251,8 +251,8 @@ width is sufficiently large."
   (set-terminal-coding-system 'utf-8)
   (setq inhibit-startup-screen t)
   (setq column-number-mode t)
-  ;; don't wrap long lines
-  (set-default 'truncate-lines t)
+  ;; wrap long lines
+  (set-default 'truncate-lines nil)
   ;; Sets the fill column (where to break paragraphs on M-q)
   (setq-default fill-column 80)
   ;; set the default font to use on all frames (see
