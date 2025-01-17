909798394b07ee2b54219f6db8ab5015c549eb21
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2025-01-17 14:41:54 +0100

    add dlv-attach dape config

--- my-conf/my-debug.el
+++ my-conf/my-debug.el
@@ -9,11 +9,17 @@
 
 
 ;; Debug Adapter Protocol (DAP) client for Emacs.
+;;
+;; Some quick
+;; Use `dape' to start a debugging session.
+;; Use `dape-kill' to stop a debugging session.
+;; Use `dape-info' to show dape windows.
+;; Use `dape-breakpoint-toggle' to set/unset breakpoints.
+;; Use `dape-breakpoint-save' to write breakpoints to a file.
+;; Use `dape-breakpoint-load' to load them.
 (use-package dape
   :straight t
-  :commands (dape dape-breakpoint-toggle)
-  :hook ((kill-emacs . dape-breakpoint-save)  ;; Save breakpoints on quit.
-         (after-init . dape-breakpoint-load)) ;; Load breakpoints on startup.
+  :commands (dape dape-info dape-breakpoint-load  dape-breakpoint-toggle)
   :config
   (setq dape-buffer-window-arrangement 'right)
   ;; Pulse source line (performance hit)
@@ -25,7 +31,23 @@
   (setq dape-display-source-buffer-action
         '((display-buffer-use-some-window display-buffer-pop-up-window)))
   ;; Save unsaved buffers when starting a `dape' session.
-  (add-hook 'dape-start-hook (lambda () (save-some-buffers t t))))
+  (add-hook 'dape-start-hook (lambda () (save-some-buffers t t)))
+  ;; Set up additional `dape-configs'.
+  ;;
+  ;; Add a "dlv-attach" config for attaching to a running debugger.
+  ;;   dlv exec --listen=localhost:33284 --headless <binary> [argv]
+  (add-to-list 'dape-configs
+	       `(dlv-attach ;; Name of configuration.
+	         modes (go-mode go-ts-mode)
+                 ensure dape-ensure-command
+	         command "dlv"
+                 command-cwd default-directory
+                 host "127.0.0.1"
+                 port 33284
+	         :type "go"
+	         :request "attach" ;; Will run "dlv attach".
+	         :mode "remote"    ;; Connect to a running debugger.
+                 :showLog "true")))
 
 
 (provide 'my-debug)
