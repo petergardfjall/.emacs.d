d864a1f85cbef0ce9b3c15b4df9c09fe69d88fc5
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2025-09-17 07:50:05 +0200

    adjust font size based on if on laptop display or not

--- init.el
+++ init.el
@@ -27,7 +27,12 @@
   "Text font to use.
 Examples include: Source Code Pro, Ubuntu Mono, Cousine, JetBrains Mono,
 Hack Nerd Font.")
-(defvar my-font-size 10.5 "Font size to use in points (for example, 10.5).")
+(defvar my-font-size (if (string-match "eDP-?1" (alist-get 'name (car (display-monitor-attributes-list))))
+                         12.0 ;; Laptop display. Appears to normally be called eDP-1 or eDP1.
+                       10.5)
+  "Font size to use in points (for example, 10.5).")
+
+
 
 ;;
 ;; Bootstrap package management.
