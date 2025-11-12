3f006471cf4af93f64d4c28983d803dcbbdc1f2b
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2025-11-12 18:44:56 +0100

    add embark-consult and wgrep for editable grep results

--- my-conf/my-search.el
+++ my-conf/my-search.el
@@ -8,6 +8,18 @@
 (message "loading %s ..." load-file-name)
 
 
+;; Incremental buffer search configured to support navigation with up/down key.
+(use-package isearch
+  :straight (:type built-in)
+  ;; Lazily load when called for.
+  :bind (("C-S-s" . isearch-forward)
+	 ("C-r"   . isearch-backward))
+  :config
+  (let ((m isearch-mode-map))
+    (define-key m (kbd "<up>") #'isearch-repeat-backward)
+    (define-key m (kbd "<down>") #'isearch-repeat-forward)))
+
+
 ;; Configure consult to provide `completing-read'-based search with live
 ;; preview.
 (use-package consult
@@ -28,16 +40,27 @@
   (setq consult-async-input-debounce 0.2))
 
 
-;; Incremental buffer search configured to support navigation with up/down key.
-(use-package isearch
-  :straight (:type built-in)
-  ;; Lazily load when called for.
-  :bind (("C-S-s" . isearch-forward)
-	 ("C-r"   . isearch-backward))
+;; The embark-consult provides the `embark-export' command which collects output
+;; candidates from `consult-grep' (or `consult-git-grep', `consult-ripgrep')
+;; into a buffer where it, for example, can be edited with `wgrep'.
+(use-package embark-consult
+  :straight t
   :config
-  (let ((m isearch-mode-map))
-    (define-key m (kbd "<up>") #'isearch-repeat-backward)
-    (define-key m (kbd "<down>") #'isearch-repeat-forward)))
+  (let ((m minibuffer-mode-map))
+    ;; Exports any candidates currently in the minibuffer to a separate buffer.
+    (define-key m (kbd "C-c e") #'embark-export)))
+
+
+;; Makes search results editable by offering in-place editing of grep-mode
+;; buffers (like ones produced by `grep' or `embark-export' (together with
+;; `consult-grep').
+;;
+;; For example, run `consult-git-grep', enter a search string, and
+;; `embark-export' to get all candidates into a separate buffer. Then "C-c C-p"
+;; (`wgrep-change-to-wgrep-mode') makes the buffer editable. Edit the search
+;; results and "C-x C-s" to save all changes.
+(use-package wgrep
+  :straight t)
 
 
 (provide 'my-search)
