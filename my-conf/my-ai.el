ac1224537925c209318a4dacba39b313b9b33f1f
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2026-03-14 08:04:23 +0100

    gptel: explaining comment

--- my-conf/my-ai.el
+++ my-conf/my-ai.el
@@ -16,9 +16,15 @@
   :commands (gptel gptel-send)
   :config
   (setq
-   ;; Gemini 2.0 Flash is free of charge with lower rate limits
-   ;; (15 requests/min and 1500 requests/day).
-   gptel-model 'gemini-2.5-flash
+   ;; Set Gemini model to use. Can change in-session with `gptel-menu' followed by `-m'.
+   ;; Gemini models are described here:
+   ;;   https://ai.google.dev/gemini-api/docs/models.
+   ;; Can also be listed with:
+   ;;   curl "https://generativelanguage.googleapis.com/v1beta/models?key=${GEMINI_API_KEY}"
+
+   ;; gptel-model 'gemini-2.5-pro   ;; Powerful model for agentic coding.
+   gptel-model 'gemini-2.5-flash ;; Well-rounded, low-latency, high-volume tasks.
+
    gptel-backend (gptel-make-gemini
                   "Gemini"
                   :key (getenv "GEMINI_API_KEY")
